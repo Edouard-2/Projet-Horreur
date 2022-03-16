99a0bb51cc9c53123c3aebb9695179f9d9a73b86
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2022-03-16 13:36:41 +0100

    Update Mat Visible Invisible

--- Assets/Scripts/Player/PlayerVision.cs
+++ Assets/Scripts/Player/PlayerVision.cs
@@ -1,14 +1,18 @@
+using System;
 using UnityEngine;
 
 public class PlayerVision : MonoBehaviour
 {
     //Changement de vision
     [SerializeField, Tooltip("Courbe de pourcentage de flou au changement de vision")] private AnimationCurve m_curveVision;
+    [SerializeField, Tooltip("Courbe de pourcentage pour la transparence du matérial")] private AnimationCurve m_curveMatVision;
+
     [SerializeField, Tooltip("Material de flou pour le postprocess")] private Material m_matVision;
     [SerializeField, Tooltip("Material des matérials Invisible en net et visible en flou")] private Material m_matInvisibleVisible;
     [SerializeField, Tooltip("Material des matérials Visible en net et Invisible en flou")] private Material m_matVisibleInvisible;
     private float m_timeVision;
-    private bool m_resetTimeVision = false;
+    private bool m_resetTimeVisionComp = false;
+    private bool m_resetTimeVisionMat = false;
 
     private void OnEnable()
     {
@@ -31,25 +35,44 @@ public class PlayerVision : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             m_timeVision = Time.time;
-            m_resetTimeVision = true;
+            m_resetTimeVisionComp = true;
+            m_resetTimeVisionMat = true;
         }
-
-        DoSwitchView();
+        float tTime = Time.time - m_timeVision;
+        DoSwitchView(tTime);
+        DoSwitchMaterial(tTime);
     }
     
-    private void DoSwitchView()
+    private void DoSwitchView(float p_time)
     {
-        float tTime = Time.time - m_timeVision;
-        if (tTime > m_curveVision.keys[m_curveVision.length-1].time)
+        if (p_time > m_curveVision.keys[m_curveVision.length-1].time)
         {
-            m_resetTimeVision = false;
+            m_resetTimeVisionComp = false;
             return;
         }
         
-        if (m_resetTimeVision)
+        if (m_resetTimeVisionComp)
         {
-            float blurValue = m_curveVision.Evaluate(tTime);
-            m_matVision.SetFloat("_BlurSize",blurValue);
+            float blurValue = m_curveVision.Evaluate(p_time);
+            m_matVision.SetFloat("_BlurSize", blurValue);
+        }
+
+        
+    }
+
+    private void DoSwitchMaterial(float p_time)
+    {
+        if (p_time > m_curveMatVision.keys[m_curveMatVision.length - 1].time)
+        {
+            m_resetTimeVisionMat = false;
+            return;
+        }
+
+        if (m_resetTimeVisionMat)
+        {
+            float matVisibilityValue = m_curveMatVision.Evaluate(p_time);
+            m_matInvisibleVisible.SetFloat("_StepStrenght", matVisibilityValue);
+            m_matVisibleInvisible.SetFloat("_StepStrenght", matVisibilityValue);
         }
     }
 }
