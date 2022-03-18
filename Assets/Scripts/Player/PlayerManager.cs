901e21e0678910413b893d9f6b3607aa6ab0967d
Edouard-2 <73876708+Edouard-2@users.noreply.github.com>
2022-03-18 14:10:53 +0100

    erz

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerManager : Singleton<PlayerManager>
 {
     //Changement de vision
-    [SerializeField, Tooltip("Courbe de pourcentage de flou au changement de vision")] private AnimationCurve m_curveVision;
-    [SerializeField, Tooltip("Courbe de pourcentage pour la transparence du matérial allé vers l'état modifié")] private AnimationCurve m_curveMatVision;
+    [SerializeField, Tooltip("Courbe de pourcentage de flou au changement de vision au début de la compétence")] private AnimationCurve m_curveVisionStart;
+    [SerializeField, Tooltip("Courbe de pourcentage de flou au changement de vision à la fin de la compétence")] private AnimationCurve m_curveVisionFinish;
+    [SerializeField, Tooltip("Courbe de pourcentage pour la transparence du matérial allé vers l'état modifié au début de la compétence")] private AnimationCurve m_curveMatVisionStart;
+    [SerializeField, Tooltip("Courbe de pourcentage pour la transparence du matérial allé vers l'état modifié à la fin de la compétence")] private AnimationCurve m_curveMatVisionFinish;
+
     [SerializeField, Tooltip("Material de flou pour le postprocess")] private Material m_matVision;
     [SerializeField, Tooltip("Material des matérials Invisible en net et visible en flou")] private Material m_matInvisibleVisible;
     [SerializeField, Tooltip("Material des matérials Visible en net et Invisible en flou")] private Material m_matVisibleInvisible;
     private float m_timeVision;
     private bool m_resetTimeVisionComp = false;
     private bool m_resetTimeVisionMat = false;
-    private int m_readyEnd = -1;
+    private int m_readyEnd = 1;
     
     //Constante
     private const float m_gravity = -9.81f;
@@ -47,27 +50,67 @@ public class PlayerManager : Singleton<PlayerManager>
         DoCursorHandler?.Invoke();
         
         //Input Blur Effect
-        IsInputDown();
+        //IsInputDown();
     }
     
     private void IsInputDown()
     {
+
+
+        float tTime = Time.time - m_timeVision;
+
         //Changement de vision
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !m_resetTimeVisionComp && !m_resetTimeVisionMat)
         {
-            m_readyEnd = 0;
+
             m_timeVision = Time.time;
+            m_readyEnd = Mathf.Abs(m_readyEnd - 1);
+
             m_resetTimeVisionComp = true;
             m_resetTimeVisionMat = true;
         }
-        
-        float tTime = Time.time - m_timeVision;
 
-        float startTimeSecMat = (m_curveVision.keys[m_curveVision.length - 1].time - m_curveMatVision.keys[m_curveMatVision.length - 1].time);
+        /*if (m_readyEnd == 0)
+        {
+            if (m_resetTimeVisionComp)
+            {
+                Debug.Log("Start");
+
+                //DoSwitchView(allé)
+                //DoSwitchView(tTime, m_curveVisionStart);
+            }
+            if (m_resetTimeVisionMat)
+            {
+                //DoSwitchMaterial(allé)
+                //DoSwitchMaterial(tTime, m_curveMatVisionStart);
+
+            }
+
+            //DoVisibleToInvisibleHandler?.Invoke();
+
+        }
+        else if (m_readyEnd == 1)
+        {
+            if (m_resetTimeVisionComp)
+            {
+                Debug.Log("Fin");
+                //DoSwitchView(retour)
+                //DoSwitchView(tTime, m_curveVisionFinish);
+            }
+            if (m_resetTimeVisionMat)
+            {
+                //DoSwitchMaterial(retour)
+                //DoSwitchMaterial(tTime, m_curveMatVisionFinish);
+            }
+
+            //DoVisibleToInvisibleHandler?.Invoke();
+        }*/
+
+
         //Vision joueur
-        DoSwitchView(tTime);
+        //DoSwitchView(tTime);
 
-        //Chanagement obj en invisible dans le flou
+        /*//Chanagement obj en invisible dans le flou
         if (m_readyEnd == 0)
         {
             //Debug.Log("hey mat 1");
@@ -81,41 +124,38 @@ public class PlayerManager : Singleton<PlayerManager>
             m_readyEnd = 1;
             m_resetTimeVisionMat = true;
             DoSwitchMaterial(tTime, m_curveMatVision, startTimeSecMat);
-        }
+        }*/
     }
     
-    private void DoSwitchView(float p_time)
+    private void DoSwitchView(float p_time, AnimationCurve p_curve)
     {
-        if (p_time > m_curveVision.keys[m_curveVision.length-1].time && m_resetTimeVisionComp)
+        if (p_time > p_curve.keys[p_curve.length-1].time && m_resetTimeVisionComp)
         {
-            m_readyEnd = -1;
-            DoVisibleToInvisibleHandler?.Invoke();
+            //DoVisibleToInvisibleHandler?.Invoke();
             m_resetTimeVisionComp = false;
             return;
         }
 
         if (m_resetTimeVisionComp)
         {
-            float blurValue = m_curveVision.Evaluate(p_time);
+            float blurValue = p_curve.Evaluate(p_time);
             m_matVision.SetFloat("_BlurSize", blurValue);
         }
     }
 
-    private void DoSwitchMaterial(float p_time, AnimationCurve p_dir, float p_minusTime)
+    private void DoSwitchMaterial(float p_time, AnimationCurve p_dir)
     {
-        float time = Mathf.Abs(p_time - p_minusTime);
-        
-        if (time > p_dir.keys[p_dir.length - 1].time && m_resetTimeVisionMat)
+        if (p_time > p_dir.keys[p_dir.length - 1].time && m_resetTimeVisionMat)
         {
             Debug.Log("Finish Material");
-            DoVisibleToInvisibleHandler?.Invoke();
+            //DoVisibleToInvisibleHandler?.Invoke();
             m_resetTimeVisionMat = false;
             return;
         }
 
         if (m_resetTimeVisionMat)
         {
-            float matVisibilityValue = p_dir.Evaluate(time);
+            float matVisibilityValue = p_dir.Evaluate(p_time);
             m_matInvisibleVisible.SetFloat("_StepStrenght", matVisibilityValue);
             m_matVisibleInvisible.SetFloat("_StepStrenght", matVisibilityValue);
         }
