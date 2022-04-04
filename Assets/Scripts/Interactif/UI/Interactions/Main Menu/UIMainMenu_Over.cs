f0ddcef5ea63801c9df84de51fd3f5e5ef9b8648
Edouard-2 <mordantedouard@gmail.com>
2022-04-04 19:09:16 +0200

    y_ç

--- Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Over.cs
+++ Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Over.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,6 +9,15 @@ public class UIMainMenu_Over : MonoBehaviour
     
     [SerializeField, Tooltip("Le textMeshPro de l'objet")]
     private TextMeshPro m_textMeshPro;
+    
+    [SerializeField, Tooltip("List de tout les waitForSeconds pour le glitch")]
+    private List<WaitForSeconds> m_listWaitForSecond;
+
+    private int m_randomIndex;
+    
+    private string m_text;
+    
+    private bool m_readyEnumerator;
     
     private void Awake()
     {
@@ -19,12 +30,30 @@ public class UIMainMenu_Over : MonoBehaviour
             {
                 Debug.Log("Il faut mettre le textMeshPro !!");
             }
+            m_text = "Error 404";
+        }
+        else
+        {
+            m_text = m_textMeshPro.text;
         }
     }
 
     private void OnMouseOver()
     {
         gameObject.transform.localScale = m_scale * 2;
+
+        if (m_readyEnumerator)
+        {
+            m_readyEnumerator = false;
+            m_randomIndex = Random.Range(2, m_listWaitForSecond.Count);
+            StartCoroutine(ActiveGlitchText());
+        }
+    }
+
+    IEnumerator ActiveGlitchText()
+    {
+        yield return m_listWaitForSecond[m_randomIndex];
+        m_readyEnumerator = true;
     }
 
     private void OnMouseExit()
