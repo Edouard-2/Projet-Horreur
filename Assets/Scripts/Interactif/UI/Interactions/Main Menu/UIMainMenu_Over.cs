f8e53232425b5bf33189e0c8b7c6cfdc6774c134
Edouard-2 <mordantedouard@gmail.com>
2022-04-04 18:55:02 +0200

    udpate

--- Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Over.cs
+++ Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Over.cs
@@ -1,9 +1,34 @@
+using TMPro;
 using UnityEngine;
 
 public class UIMainMenu_Over : MonoBehaviour
 {
+    private Vector3 m_scale;
+    
+    [SerializeField, Tooltip("Le textMeshPro de l'objet")]
+    private TextMeshPro m_textMeshPro;
+    
+    private void Awake()
+    {
+        m_scale = gameObject.transform.localScale;
+
+        if (m_textMeshPro == null)
+        {
+            m_textMeshPro = GetComponent<TextMeshPro>();
+            if (m_textMeshPro == null)
+            {
+                Debug.Log("Il faut mettre le textMeshPro !!");
+            }
+        }
+    }
+
     private void OnMouseOver()
     {
-        
+        gameObject.transform.localScale = m_scale * 2;
     }
-}
+
+    private void OnMouseExit()
+     {
+         gameObject.transform.localScale = Vector3.one;
+     }
+ }
