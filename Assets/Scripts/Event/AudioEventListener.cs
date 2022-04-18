e5ba81e3950a28c0d3b79eaea9d5beccfaa89b35
Edouard-2 <mordantedouard@gmail.com>
2022-04-18 16:44:08 +0200

    Updaet

---
+++ Assets/Scripts/Event/AudioEventListener.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using FMODUnity;
+
+public class AudioEventListener : MonoBehaviour
+{
+    [SerializeField,Tooltip("L'event qui déclenchera le son")]private EventsTrigger m_event;
+    [SerializeField,Tooltip("Le composent sound emitter")]private StudioEventEmitter m_soundEmitter;
+
+    private void OnEnable()
+    {
+        m_event.OnTrigger += StartSound;
+    }
+
+    private void OnDisable()
+    {
+        m_event.OnTrigger += StartSound;
+    }
+
+    private void Awake()
+    {
+        if (m_soundEmitter == null)
+        {
+            m_soundEmitter = GetComponent<StudioEventEmitter>();
+            if (m_soundEmitter == null)
+            {
+                Debug.LogError("Il faut mettre le SoundEmitter !!!", this);
+            }
+        }
+    }
+
+    private void StartSound(bool p_start = true)
+    {
+        m_soundEmitter.Play();
+    }
+}
