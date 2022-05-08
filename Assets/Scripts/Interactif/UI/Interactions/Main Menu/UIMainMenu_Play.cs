0b863e9de6f58bed6dc0d7aa14fd504a6aa92602
Edouard-2 <mordantedouard@gmail.com>
2022-05-08 19:20:12 +0200

    Updaet

--- Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Play.cs
+++ Assets/Scripts/Interactif/UI/Interactions/Main Menu/UIMainMenu_Play.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,9 +8,35 @@ public class UIMainMenu_Play : MonoBehaviour
     [SerializeField, Tooltip("Index de la scene pour commencer le jeu")] 
     private int m_sceneIndex;
     
+    [SerializeField, Tooltip("Index de la scene pour commencer le jeu")] 
+    private Animator m_fadeAnimator;
+
+    private WaitForSeconds m_waitUntilLoadScene = new WaitForSeconds(0.5f);
+    
     private void OnMouseDown()
     {
         Debug.Log("Over Text");
-        SceneManager.LoadSceneAsync(m_sceneIndex);
+        //Lanacer l'animation du fond noir
+        m_fadeAnimator.SetTrigger(Animator.StringToHash("FadeIn"));
+
+        StartCoroutine(WaitUntilLaunchLoadingScene());
+    }
+
+    IEnumerator WaitUntilLaunchLoadingScene()
+    {
+        yield return m_waitUntilLoadScene;
+        //Lancement de la scene principale
+        StartCoroutine(LoadAsynScene(m_sceneIndex));
+    }
+    
+    IEnumerator LoadAsynScene(int p_sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(p_sceneIndex);
+        
+        while (!operation.isDone)
+        {
+            
+            yield return null;
+        }
     }
 }
